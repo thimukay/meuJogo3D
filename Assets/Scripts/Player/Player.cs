3dcea2f5e4260063f45499cac65e6865221a22e8
thimukay <58257905+thimukay@users.noreply.github.com>
2024-04-06 15:51:03 +0200

    updating Player.cs with Jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,19 +7,31 @@ public class Player : MonoBehaviour
     public CharacterController characterController;
     public float speed = 1f; 
     public float turnSpeed = 1f; 
-    public float gravity = -9.8f; 
+    public float gravity = 9.8f; 
     private float vSpeed = 0f;
 
     public Animator animator;
+
+    public float jumpSpeed = 15f;
+    public KeyCode jumpKeyCode = KeyCode.Space;
 
 
     void Update() 
     { 
         transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0); 
         var inputAxisVertical = Input.GetAxis("Vertical"); 
-        var speedVector = transform.forward * inputAxisVertical * speed; 
+        var speedVector = transform.forward * inputAxisVertical * speed;
 
-        vSpeed += gravity * Time.deltaTime; 
+        if (characterController.isGrounded) 
+        { 
+            vSpeed = 0; 
+            if (Input.GetKeyDown(jumpKeyCode)) 
+            { 
+                vSpeed = jumpSpeed; 
+            } 
+        }
+
+        vSpeed -= gravity * Time.deltaTime; 
         speedVector.y = vSpeed; 
         //
         characterController.Move(speedVector * Time.deltaTime);
