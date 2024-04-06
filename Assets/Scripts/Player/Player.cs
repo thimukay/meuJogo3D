0036ab1db71a4a8a91d3f4387bd0279d151e81dc
thimukay <58257905+thimukay@users.noreply.github.com>
2024-04-06 15:43:31 +0200

    adding Player Script and adding cinemachine vcam

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController characterController;
+    public float speed = 1f; 
+    public float turnSpeed = 1f; 
+    public float gravity = -9.8f; 
+    private float vSpeed = 0f;
+
+    public Animator animator;
+
+
+    void Update() 
+    { 
+        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0); 
+        var inputAxisVertical = Input.GetAxis("Vertical"); 
+        var speedVector = transform.forward * inputAxisVertical * speed; 
+
+        vSpeed += gravity * Time.deltaTime; 
+        speedVector.y = vSpeed; 
+        //
+        characterController.Move(speedVector * Time.deltaTime);
+
+        animator.SetBool("Run", inputAxisVertical != 0);
+    }
+}
