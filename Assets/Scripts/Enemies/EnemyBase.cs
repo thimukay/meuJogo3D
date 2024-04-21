71e29317b80af277ad2cf77da95597b362472363
thimukay <58257905+thimukay@users.noreply.github.com>
2024-04-21 14:35:45 +0200

    changing EnemyBase and adding box collider

--- Assets/Scripts/Enemies/EnemyBase.cs
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 
 namespace Enemy
 {
@@ -10,6 +10,11 @@ namespace Enemy
         public float startLife = 10f;
 
         [SerializeField] private float _currentLife;
+
+        [Header("Start Animation")]
+        public float startAnimationDuration = .2f;
+        public Ease startAnimationEase = Ease.OutBack;
+        public bool startWithBornAnimation = true;
 
         private void Awake()
         {
@@ -24,6 +29,10 @@ namespace Enemy
         protected virtual void Init()
         {
             ResetLife();
+            if(startWithBornAnimation)
+            {
+                BornAnimation();
+            }
         }
 
         protected virtual void Kill()
@@ -44,6 +53,13 @@ namespace Enemy
                 Kill();
             }
         }
+
+        #region ANIMATION
+        private void BornAnimation()
+        {
+            transform.DOScale(0, startAnimationDuration).SetEase(startAnimationEase).From();
+        }
+        #endregion
 
         // debug
         private void Update()
