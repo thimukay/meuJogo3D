c29849b107bcbb669e9197513935e31b388adfd0
thimukay <58257905+thimukay@users.noreply.github.com>
2024-04-27 17:29:07 +0200

    adding EnemyWalk script, adding to enemy02

---
+++ Assets/Scripts/Enemies/EnemyWalk.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyWalk : EnemyBase
+    {
+        [Header("WayPoints")]
+        public GameObject[] wayPoints;
+        public float minDistance = 1f;
+        public float speed = 1f;
+
+        private int _index = 0;
+        
+        
+        // debug
+        private void Update()
+        {
+            if (Vector3.Distance(transform.position, wayPoints[_index].transform.position) < minDistance)
+            {
+                _index++;
+                if(_index >= wayPoints.Length)
+                {
+                    _index = 0;
+                }
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, wayPoints[_index].transform.position, Time.deltaTime * speed);
+        }
+    }
+}
+
