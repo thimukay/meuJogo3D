0396c5138ee91d69bb77129be46cadcba40c8e92
thimukay <58257905+thimukay@users.noreply.github.com>
2024-06-04 17:22:52 +0200

    adding clothManager and removing mesh from the guns

---
+++ Assets/Scripts/Cloth/ClothManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ebac.Core.Singleton;
+
+
+namespace Cloth
+{
+
+    public enum ClothType
+    {
+        SPEED,
+        TEST
+    }
+    public class ClothManager : Singleton<ClothManager>
+    {
+        public List<ClothSetup> clothSetups;
+
+        public ClothSetup GetSetupByType(ClothType cloth)
+        {
+            return clothSetups.Find(i => i.clothType == cloth);
+        }
+    }
+
+    [System.Serializable]
+    public class ClothSetup
+    {
+        public ClothType clothType;
+        public Texture2D texture;
+    }
+}
+
